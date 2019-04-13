ee802f0fd6a9e6c786a08a69dcfab77be9ca7c77
mehulpoddar <mehulpoddar@gmail.com>
2019-04-13 09:38:27 +0530

    reccomender

---
+++ NoineFlow/Recommender.py
@@ -0,0 +1,13 @@
+def recommend(id):
+
+    tags = []
+    dat = pd.read_csv("../NoineEngine/Questions_Rank.csv", encoding='latin1')
+    for i in range(len(dat['ID'])):
+        if id == dat.iloc(i, 0):
+            tags = dat.iloc(i, 3).split(',')
+
+    res = pd.read_csv("../NoineEngine/Resources.csv", encoding='latin1')
+    for i in range(len(res['Links'])):
+        for j in tags:
+            if j == res.iloc(i, 1):
+                return res.iloc(i,0);
